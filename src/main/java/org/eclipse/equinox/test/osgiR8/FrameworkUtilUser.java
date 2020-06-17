021c5158b7e49328a67637e9133acaad5e5a4acd
Thomas Watson <tjwatson@us.ibm.com>
2020-06-17 07:51:31 -0500

    Add toolchain for Java 7

--- src/main/java/org/eclipse/equinox/test/osgiR8/FrameworkUtilUser.java
+++ src/main/java/org/eclipse/equinox/test/osgiR8/FrameworkUtilUser.java
@@ -7,4 +7,9 @@ public class FrameworkUtilUser {
 	Bundle getBundle(Object o) {
 		return FrameworkUtil.getBundle(o.getClass());
 	}
+
+	// Compiling this method against Java 7 will fail with missing Optional
+//	Bundle getBundleByClassLoader(ClassLoader loader) {
+//		return FrameworkUtil.getBundle(loader).get();
+//	}
 }
